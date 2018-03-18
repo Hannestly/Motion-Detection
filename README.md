08a2a82e66507c2ea94df8d45734bb2e1cbba6b5
Hyon Lee <dooley994@gmail.com>
2018-03-18 17:12:48 -0400

    update on readme

--- README.md
+++ README.md
@@ -18,3 +18,5 @@ vid = cv2.VideoCapture('address of your video')
 ```
 - compile the motion.py 
 
+### Demo ###
+<a href="https://gyazo.com/5a719ff04dead4a8717946ed5e3a5d9e"><img src="https://i.gyazo.com/5a719ff04dead4a8717946ed5e3a5d9e.gif" alt="https://gyazo.com/5a719ff04dead4a8717946ed5e3a5d9e" width="782"/></a>
--- motion.py
+++ motion.py
@@ -2,10 +2,14 @@ import cv2
 import numpy as np 
 import imutils
  
+vid = cv2.VideoCapture(r'C:\Users\100488516\Desktop\PythonProgram\motionDetec\vid1.mp4') #Calling the source video as the variable 'vid
+prev_frame = None #empty variable to contain the previous frame
 
-vid = cv2.VideoCapture(0)
-prev_frame = None
-
+'''
+checkMoving function takes in three arguments -difference frame, dimension of the difference frame, and the original frame (colored).
+for every 20X20 pixels square in the difference frame, if the sum of all the pixel's grey value is different than 200 * 9, it is asuumed
+that the movement is detected. 
+'''
 def checkMoving(frame,dimension,ogframe):
     x1 = 0
     y1 = 0
@@ -15,20 +19,22 @@ def checkMoving(frame,dimension,ogframe):
         while x1 +20 <= dimension[0]:
 
             frameCheck = frame[x1:x1+20,y1:y1+20]
-            if frameCheck.sum() > 83000 or frameCheck.sum() < 79500:
+            if frameCheck.sum() > 82000 or frameCheck.sum() < 79000:
                 print("moving",x1,y1)
                 print(ogframe.shape)
                 cv2.rectangle(ogframe,(y1,x1),(y1+20,x1+20),(0,0,255),2)                
             x1 +=20
         y1 += 20 
 
+
             
 
 while(True):
-    ret,frame = vid.read()
-    frame = imutils.resize(frame,width= 800)
-    gray = cv2.cvtColor(frame,cv2.COLOR_BGR2GRAY)
-    neg = gray + 200
+    
+    ret,frame = vid.read() #'ret' contains boolean value - whether it has next frame, 'frame' is a numpy matrix containing pixels with BGR value
+    frame = imutils.resize(frame,width= 800) #Using imutils library to shrink the video for faster computation
+    gray = cv2.cvtColor(frame,cv2.COLOR_BGR2GRAY)  #motion detection does not require color, other than grayscale
+    neg = gray + 200   #working with value near 0 has issues calculating since 0 -1 = 255 in pixel calculation
 
     if prev_frame is not None:
 

e31fd1ec1680583f9e970fc682f07b62d63a5927
Hyon Lee <dooley994@gmail.com>
2018-03-24 21:34:14 -0400

    changing scaling algorithm into recursive

--- motion.py
+++ motion.py
@@ -2,7 +2,7 @@ import cv2
 import numpy as np  
 import imutils
  
-vid = cv2.VideoCapture('vid1.mp4') #Calling the source video as the variable 'vid
+vid = cv2.VideoCapture('snow.mp4') #Calling the source video as the variable 'vid
 prev_frame = None #empty variable to contain the previous frame
 
 '''
@@ -10,6 +10,37 @@ checkMoving function takes in three arguments -difference frame, dimension of th
 for every 20X20 pixels square in the difference frame, if the sum of all the pixel's grey value is different than 200 * 9, it is asuumed
 that the movement is detected. 
 '''
+
+def overlap(recList, length):
+    if length == 1:
+        return recList
+    if len(recList) == length:
+        return recList
+    else: 
+        x1 = recList[0][0]
+        y1 = recList[0][1]
+        x2 = recList[0][2]
+        y2 = recList[0][3]
+        index = 1
+        ogLength = len(recList)
+        while index < ogLength:
+            if x1-70 < recList[index][0] and y1-70 < recList[index][1] and x2+70 > recList[index][0] and y2+70 > recList[index][1]:
+                if x1 > recList[index][0]:
+                    x1 = recList[index][0]
+                if y1 > recList[index][1]:
+                    y1 = recList[index][1]
+                if x2 < recList[index][2]:
+                    x2 = recList[index][2]
+                if y2 < recList[index][3]:
+                    y2 = recList[index][3]
+                index += 1
+            else:
+                recList.append(recList[index])
+                index += 1
+        recList.append([x1,y1,x2,y2])
+        recList = recList[ogLength:]
+        return(recList,ogLength)
+
 
 def checkMoving(frame,dimension,ogframe):
     rect = []
@@ -19,29 +50,25 @@ def checkMoving(frame,dimension,ogframe):
         x = 0
         while x +20 <= dimension[1]:
             frameCheck = frame[y:y+20,x:x+20]
-            if frameCheck.sum() > 80400 or frameCheck.sum() < 79000:
+            if frameCheck.sum() > 81000 or frameCheck.sum() < 79000:
                 rect.append([x,y,x+20,y+20])
             x +=20
-        y += 20 
+        y += 20
 
     if len(rect) != 0:
-        x1 = rect[0][0]
-        y1 = rect[0][1]
-        x2 = rect[0][2]
-        y2 = rect[0][3]
-        index = 1
-        while index < len(rect):
-            if rect[index][0] < x1:
-                x1 = rect[index][0]
-            if rect[index][1] < y1:
-                y1 = rect[index][1]
-            if rect[index][2] > x2:
-                x2 = rect[index][2]
-            if rect[index][3] > y2:
-                y2 = rect[index][3]
-            index +=1
+        if len(rect) == 1:
+            cv2.rectangle(ogframe,(rect[0][0],rect[0][1]),(rect[0][2],rect[0][3]),(0,0,255),2)
+        else:
+            rectangles = overlap(rect,0)
+            for i in range(0,len(rectangles[0])):
+                cv2.rectangle(ogframe,(rectangles[0][i][0],rectangles[0][i][1]),(rectangles[0][i][2],rectangles[0][i][3]),(0,0,255),2)
+            
+    
+            
+                    
+
+            
         
-        cv2.rectangle(ogframe,(x1,y1),(x2,y2),(0,0,255),2) 
 
             
 
